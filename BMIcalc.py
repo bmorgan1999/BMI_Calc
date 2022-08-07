3adc52372e751ef9fbaacb550d00a4a21240ea88
Braxton Morgan <110731404+bmorgan1999@users.noreply.github.com>
2022-08-06 21:10:53 -0400

    Add files via upload

---
+++ BMIcalc.py
@@ -0,0 +1,14 @@
+name = "braxton"
+weight_kg = 73
+height_m = 1.8
+
+bmi = weight_kg / (height_m * height_m)
+print("bmi: ")
+print(bmi)
+if bmi < 25:
+    print("is not overweight")
+else:
+    print("braxton is over the amount and is overweight")
+
+
+
